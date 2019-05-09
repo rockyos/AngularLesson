ba3f064b838220742e3fba43709dc8e5218572b2
rockyos <46592602+rockyos@users.noreply.github.com>
2019-05-09 09:24:53 +0300

    Start

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# AngularTest
+# AngularLesson
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.2.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.5.
 
 ## Development server
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularTest'`, () => {
+  it(`should have as title 'AngularLesson'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('AngularTest');
+    expect(app.title).toEqual('AngularLesson');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to AngularTest!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to AngularLesson!');
   });
 });
